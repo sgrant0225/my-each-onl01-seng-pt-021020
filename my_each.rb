0ea4819f46cb7e3cff4ac0c5943ad1c354dacfa9
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-17 04:10:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'pry'
 def my_each(words)
   i = 0
@@ -6,5 +7,12 @@ def my_each(words)
     i += 1
   end
   words
+=======
+def my_each(words)
+  i = 0
+ while i < words.length
+  i += 1
+  end
+>>>>>>> afa417adee8ebee4b1322cac5b1ac6a7451baff6
 end
     
